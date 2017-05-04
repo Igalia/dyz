5b5f27eafed291f8fbbcf6fa091c9e969babab91
Zan Dobersek <zdobersek@igalia.com>
2017-05-04 09:32:12 +0200

    src/main.lua: convert the argv arguments into an array, pass it to singleview.run().

--- src/main.lua
+++ src/main.lua
@@ -1,6 +1,17 @@
+local ffi = require("ffi")
 local singleview = require("apps.singleview.main")
 
-function main (args)
+ffi.cdef[[
+    extern int argc;
+    extern char** argv;
+]]
+
+function main ()
+    local args = {}
+    for i = 1, ffi.C.argc - 1 do
+        table.insert(args, ffi.string(ffi.C.argv[i]))
+    end
+
     singleview.run(args)
 end
 
