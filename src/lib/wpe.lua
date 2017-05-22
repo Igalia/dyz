cacc6b0ca90b5d0aa5df2fb7e507b55c2dc68c08
Zan Dobersek <zdobersek@igalia.com>
2017-05-22 19:48:05 +0200

    Remove src/lib/wpewebkit.lua.

--- src/lib/wpe.lua
+++ src/lib/wpe.lua
@@ -1,5 +1,42 @@
 local ffi = require "ffi"
-local wk = require "lib.wpewebkit"
+
+local wk = ffi.load("libWPEWebKit.so")
+
+ffi.cdef[[
+    typedef const struct OpaqueWKString* WKStringRef;
+    typedef const struct OpaqueWKContext* WKContextRef;
+    typedef const struct OpaqueWKPageGroup* WKPageGroupRef;
+    typedef const struct OpaqueWKPageConfiguration* WKPageConfigurationRef;
+    typedef const struct OpaqueWKView* WKViewRef;
+    typedef const struct OpaqueWKPage* WKPageRef;
+    typedef const struct OpaqueWKURL* WKURLRef;
+
+    WKStringRef WKStringCreateWithUTF8CString(const char* string);
+    size_t WKStringGetMaximumUTF8CStringSize(WKStringRef);
+    size_t WKStringGetUTF8CString(WKStringRef, char* buffer, size_t bufferSize);
+
+    WKContextRef WKContextCreate();
+
+    WKPageGroupRef WKPageGroupCreateWithIdentifier(WKStringRef identifier);
+    WKPageConfigurationRef WKPageConfigurationCreate();
+
+    void WKPageConfigurationSetContext(WKPageConfigurationRef configuration, WKContextRef context);
+    void WKPageConfigurationSetPageGroup(WKPageConfigurationRef configuration, WKPageGroupRef pageGroup);
+
+    WKViewRef WKViewCreate(WKPageConfigurationRef);
+    WKPageRef WKViewGetPage(WKViewRef);
+    void WKPageLoadURL(WKPageRef page, WKURLRef url);
+
+    WKURLRef WKURLCreateWithUTF8CString(const char* string);
+    WKStringRef WKURLCopyString(WKURLRef);
+    WKStringRef WKURLCopyHostName(WKURLRef);
+    WKStringRef WKURLCopyScheme(WKURLRef);
+    WKStringRef WKURLCopyPath(WKURLRef);
+    WKStringRef WKURLCopyLastPathComponent(WKURLRef);
+    bool WKURLIsEqual(WKURLRef, WKURLRef other);
+
+    void WKRelease(void*);
+]]
 
 local function gc_release(object)
     wk.WKRelease(ffi.gc(object, nil))
