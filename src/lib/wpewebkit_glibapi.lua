471eeac8a9786b74c43c39d5c1b02d0418a3b236
Carlos Alberto Lopez Perez <clopez@igalia.com>
2017-09-26 17:32:35 +0200

    Enable some run-time settings by default

--- src/lib/wpewebkit_glibapi.lua
+++ src/lib/wpewebkit_glibapi.lua
@@ -4,7 +4,11 @@ local wk = ffi.load("libWPEWebKit.so")
 
 ffi.cdef[[
     typedef struct _WebKitWebView WebKitWebView;
+    typedef struct _WebKitSettings WebKitSettings;
 
+    WebKitSettings* webkit_settings_new_with_settings (const char* first_setting_name, ...);
+
+    WebKitWebView* webkit_web_view_new_with_settings  (WebKitSettings* settings);
     WebKitWebView* webkit_web_view_new ();
     void webkit_web_view_load_uri (WebKitWebView* web_view, const char* uri);
 ]]
