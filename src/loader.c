a5784e16b9b60c85b6bf78908faa0ed0a2ce0473
Zan Dobersek <zdobersek@igalia.com>
2018-01-02 18:47:12 +0100

    Add a relay libWPEBackend-dyz.so library.

---
+++ src/loader.c
@@ -0,0 +1,29 @@
+#include <wpe/loader.h>
+
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void* s_relay_library = 0;
+static struct wpe_loader_interface* s_relay_loader = 0;
+
+void* relay_load_object(const char* object_name)
+{
+    if (!s_relay_library) {
+        s_relay_library = dlopen("libWPEBackend-fdo.so", RTLD_NOW);
+        if (s_relay_library)
+            s_relay_loader = dlsym(s_relay_library, "_wpe_loader_interface");
+    }
+
+    if (!s_relay_library || !s_relay_loader) {
+        fprintf(stderr, "libWPEBackend-dyz: could not load libWPEBackend-fdo\n");
+        abort();
+    }
+
+    return s_relay_loader->load_object(object_name);
+}
+
+__attribute__((visibility("default")))
+struct wpe_loader_interface _wpe_loader_interface = {
+    relay_load_object,
+};
